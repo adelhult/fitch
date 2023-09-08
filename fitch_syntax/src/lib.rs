e2ae1994cc91df87b7136de53295719a4466c981
Eli Adelhult <eli.adelhult@outlook.com>
2023-09-08 23:16:53 +0200

    Add proposition parser

--- fitch_syntax/src/lib.rs
+++ fitch_syntax/src/lib.rs
@@ -5,8 +5,8 @@ use fitch_core::{Prop, Rule};
 pub enum Command {
     Rule(String), // TODO: Rule
     Copy(usize),
-    Premise(String), // TODO Prop
-    Assume(String),  // TODO Prop
+    Premise(Prop),
+    Assume(Prop),
     Discharge,
     Quit,
     // TODO: Revert, LaTeX, Exit
@@ -16,36 +16,30 @@ pub fn parse_command(s: &str) -> Command {
     command().parse(s).unwrap()
 }
 
-fn index() -> impl Parser<char, usize, Error = Simple<char>> {
-    text::int(10).map(|n: String| n.parse().unwrap())
-}
-
 fn command() -> impl Parser<char, Command, Error = Simple<char>> {
-    let rule = text::keyword("rule")
+    let rule = just("rule")
         .ignore_then(whitespace())
         .ignore_then(text::ident()) // TODO: Add a rule parser
         .map(Command::Rule);
 
-    let copy = text::keyword("copy")
+    let copy = just("copy")
         .ignore_then(whitespace())
         .ignore_then(index())
         .map(Command::Copy);
 
-    let premise = text::keyword("premise")
+    let premise = just("premise")
         .ignore_then(whitespace())
-        .ignore_then(text::ident()) // TODO: Add a prop parser
+        .ignore_then(prop())
         .map(Command::Premise);
 
-    let assume = text::keyword("assume")
+    let assume = just("assume")
         .ignore_then(whitespace())
-        .ignore_then(text::ident()) // TODO: Add a prop parser
+        .ignore_then(prop())
         .map(Command::Premise);
 
-    let discharge = text::keyword("discharge").map(|_| Command::Discharge);
+    let discharge = just("discharge").map(|_| Command::Discharge);
 
-    let quit = text::keyword("quit")
-        .or(text::keyword("exit"))
-        .map(|_| Command::Quit);
+    let quit = just("quit").or(just("exit")).map(|_| Command::Quit);
 
     rule.or(copy)
         .or(premise)
@@ -53,4 +47,50 @@ fn command() -> impl Parser<char, Command, Error = Simple<char>> {
         .or(discharge)
         .or(quit)
         .padded()
+        .then_ignore(end())
+}
+
+fn index() -> impl Parser<char, usize, Error = Simple<char>> {
+    text::int(10).map(|n: String| n.parse().unwrap())
+}
+
+fn prop() -> impl Parser<char, Prop, Error = Simple<char>> {
+    recursive(|prop| {
+        let bottom = just("bottom").or(just("⊥")).map(|_| Prop::Bottom);
+        let symbol = text::ident().map(Prop::Symbol);
+
+        let atom = bottom
+            .or(symbol)
+            .or(prop.delimited_by(just('('), just(')')));
+
+        let negate_op = just('-').or(just('¬')).padded();
+        let negate = negate_op
+            .repeated()
+            .then(atom)
+            // Note negation is implemented as -phi = phi -> bottom
+            .foldr(|_op, rhs| Prop::Imply(Box::new(rhs), Box::new(Prop::Bottom)));
+
+        let and_op = just('*').or(just('∧')).or(just('&')).padded();
+        let and = negate
+            .clone()
+            .then(and_op.ignore_then(negate).repeated())
+            .foldl(|lhs, rhs| Prop::And(Box::new(lhs), Box::new(rhs)));
+
+        let or_op = just('+').or(just('∨')).or(just('|').or(just('v'))).padded();
+        let or = and
+            .clone()
+            .then(or_op.ignore_then(and).repeated())
+            .foldl(|lhs, rhs| Prop::Or(Box::new(lhs), Box::new(rhs)));
+
+        let implies_op = just("->").or(just("⇒")).or(just("→")).padded();
+        let implies = or
+            .clone()
+            .then_ignore(implies_op)
+            .repeated()
+            .then(or)
+            .foldr(|lhs, rhs| Prop::Imply(Box::new(lhs), Box::new(rhs)));
+
+        implies
+    })
+    // TODO: Maybe add a .then_ignore(end())
 }
